7b0ef11349b2dcba8f3b31155e1318453ce0f6cf
yubangbing <1640184819@qq.com>
2017-05-11 23:17:17 +0800

    ds

---
+++ rabbitmq/src/com/aitongyi/rabbit/helloworld/P.java
@@ -0,0 +1,35 @@
+package com.aitongyi.rabbit.helloworld;
+import com.rabbitmq.client.Channel;  
+import com.rabbitmq.client.Connection;  
+import com.rabbitmq.client.ConnectionFactory;  
+  
+/**  
+ * 消息生产者  
+ *   
+ * @author hushuang  
+ *   
+ */  
+public class P {  
+  
+    private final static String QUEUE_NAME = "hello";  
+  
+    public static void main(String[] argv) throws Exception {  
+        // 创建连接工厂  
+        ConnectionFactory factory = new ConnectionFactory();  
+//      设置RabbitMQ地址  
+        factory.setHost("localhost");  
+//      创建一个新的连接  
+        Connection connection = factory.newConnection();  
+//      创建一个频道  
+        Channel channel = connection.createChannel();  
+//      声明一个队列 -- 在RabbitMQ中，队列声明是幂等性的（一个幂等操作的特点是其任意多次执行所产生的影响均与一次执行的影响相同），也就是说，如果不存在，就创建，如果存在，不会对已经存在的队列产生任何影响。   
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);  
+        String message = "Hello World!";  
+//      发送消息到队列中  
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));  
+        System.out.println("P [x] Sent '" + message + "'");  
+//      关闭频道和连接  
+        channel.close();  
+        connection.close();  
+    }  
+}  
---
+++ rabbitmq/src/com/aitongyi/rabbit/queues/Worker1.java
@@ -0,0 +1,47 @@
+package com.aitongyi.rabbit.queues;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.*;
+
+public class Worker1 {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println("Worker1 [*] Waiting for messages. To exit press CTRL+C");
+        // 每次从队列中获取数量
+        channel.basicQos(1);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+
+                System.out.println("Worker1 [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } finally {
+                    System.out.println("Worker1 [x] Done");
+                    // 消息处理完成确认
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            }
+        };
+        // 消息消费完成确认
+        channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+    }
+
+    private static void doWork(String task) {
+        try {
+            Thread.sleep(500); // 暂停1秒钟
+        } catch (InterruptedException _ignored) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
